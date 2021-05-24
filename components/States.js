bd6da7eede469eb38e20c0133609d7642c5cd15d
osxpc <h4ck3r@osxpcs-iMac.local>
2021-05-24 10:52:20 -0700

    fetching resources

--- components/States.js
+++ components/States.js
@@ -2,41 +2,55 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 const States = ({ placeholder }) => {
   // ------------ useState takes the initial value than will be changed afterwards
-  const [age, setAge] = useState(25);
-  let [counter, setCounter] = useState(1);
-  const [blogs, setBlogs] = useState([
-    { title: 'A title', body: 'lorem ipsum dolor ...', author: 'Mario', id: '1' },
-    { title: 'A title', body: 'lorem ipsum dolor ...', author: 'Luigi', id: '2' },
-    { title: 'A title', body: 'lorem ipsum dolor ...', author: 'Gonzalo', id: '3' },
-    { title: 'A title', body: 'lorem ipsum dolor ...', author: 'Aquije', id: '4' },
-    { title: 'A title', body: 'lorem ipsum dolor ...', author: 'Alvaro', id: '5' },
-  ]);
-
+  //Null bc we are using an API
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  // -----------------
   const [name, setName] = useState('mario');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blogs) => blogs.id !== id);
-    setBlogs(newBlogs);
-  };
   // This will run every time somethiing is rendered again, states
   useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
     console.log(name);
-    // The empry array will make the function only to run on THE FIRST LOAD
-  }, [name]);
+    setTimeout(() => {
+      // Fetching data -- can't use ASYNC inside useEffect
+      fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          return res.status == 200 ? res.json() : console.log('Failed to fetch');
+        })
+
+        // Loading messages and errors may show here
+        .then((data) => {
+          setBlogs(data);
+          console.log(data);
+          setIsPending(false);
+        })
+        .catch((err) => {
+          console.error(err.message);
+        });
+    }, 1000);
+
+    // The empty array will make the function only to run on THE FIRST LOAD
+  }, []);
   // When smth is passed into the array the hook will look for it on changes
 
   return (
     <div>
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}></BlogList>
-
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === 'Mario' || blog.author === 'mario')}
-        title="Mario's Blogs"
-        handleDelete={handleDelete}></BlogList>
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{name}</p>
+      {/* Conditional templating, ONLY SHOWS RIGHT IF LEFT IS TRUE */}
+      {isPending && (
+        <div className="text-center items-center text-lg font-semibold animate-ping translate-y-10">
+          Loading ...
+        </div>
+      )}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!"></BlogList>}
+      {blogs && (
+        <BlogList
+          blogs={blogs.filter(
+            (blog) => blog.author === 'Mario' || blog.author === 'mario'
+          )}
+          title="Mario's Blogs"></BlogList>
+      )}
     </div>
   );
 };
